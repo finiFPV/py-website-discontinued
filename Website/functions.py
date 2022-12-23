ecc225297182740878631b89a597ba8d6450a804
fininimini <markusskalenda@gmail.com>
2022-12-24 01:02:49 +0200

    password requirments

--- Website/functions.py
+++ Website/functions.py
@@ -2,33 +2,40 @@ class DB:
     def __init__(self, collection="Users"):
         from pymongo import MongoClient
         self.client = MongoClient("***REMOVED***")["Website"][collection]
+
     def search_user(self, username):
-        results = self.client.find({'user': username})
+        results = self.client.find({"user": username})
         results = [credentials for credentials in results]
         if len(results) > 0:
             user_found = True
             credentials = results[0]
-        else: 
+        else:
             user_found = False
             credentials = {}
         return user_found, credentials
+
     def insert_data(self, username, password):
-        self.client.insert_one({'user': username, 'pswd': password})
+        self.client.insert_one({"user": username, "pswd": password})
+
 
 class Account:
     class Main:
         @staticmethod
         def login(username, password):
             results = DB().search_user(username)
-            if results[0] == False: return 404
-            elif results[1]['pswd'] == password: return results
-            else: return 401
+            if results[0] == False:
+                return 404
+            elif results[1]["pswd"] == password:
+                return results
+            else:
+                return 401
+
         @staticmethod
         def register(username, password):
             results = DB().search_user(username)
             if results[0] == False:
                 DB().insert_data(username, password)
-                if DB().search_user(username)[1]['user'] == username:
+                if DB().search_user(username)[1]["user"] == username:
                     return 201
                 else:
                     return 404
--- Website/pages.py
+++ Website/pages.py
@@ -2,80 +2,89 @@ from flask import Blueprint, render_template, redirect, request, make_response, 
 from functions import Account
 from json import dumps
 
-app = Blueprint('app', 'app')
+app = Blueprint("app", "app")
 Account = Account()
 Main = Account.Main()
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def main():
-    if 'Credentials' in request.cookies:
+    if "Credentials" in request.cookies:
         return make_response(f"Logged in! (Work in progress...)")
-    else: 
-        return redirect('/login')
+    else:
+        return redirect("/login")
 
-@app.route('/login')
+
+@app.route("/login")
 def login_page():
-    if 'Credentials' in request.cookies:
-        return redirect('/')
-    error = session.get('error')
-    success = session.get('success')
-    return render_template('login.html', error = error, success = success)
+    if "Credentials" in request.cookies:
+        return redirect("/")
+    error = session.get("error")
+    success = session.get("success")
+    return render_template("login.html", error=error, success=success)
 
-@app.route('/register')
+
+@app.route("/register")
 def register():
-    if 'Credentials' in request.cookies:
-        return redirect('/')
-    error = session.get('error')
-    success = session.get('success')
-    return render_template('register.html', error = error, success = success)
+    if "Credentials" in request.cookies:
+        return redirect("/")
+    error = session.get("error")
+    success = session.get("success")
+    return render_template("register.html", error=error, success=success)
 
-@app.route('/handle_data', methods=['POST'])
+
+@app.route("/handle_data", methods=["POST"])
 def handle_data():
     def reset_message():
-        try: 
-            for i in ['error', 'success']:
+        try:
+            for i in ["error", "success"]:
                 if session[i] != None:
                     session[i] = None
-        except: pass
-    method = request.form['method']
+        except:
+            pass
 
-    if method == 'Register':
+    method = request.form["method"]
+
+    if method == "Register":
         reset_message()
-        return redirect('/register')
+        return redirect("/register")
 
-    elif method == 'Back':
+    elif method == "Back":
         reset_message()
-        return redirect('/login')
+        return redirect("/login")
 
-    elif method == 'Login':
-        user = request.form['user']
-        if user == '':
-            return redirect('/login')
-        password = request.form['pass']
+    elif method == "Login":
+        user = request.form["user"]
+        if user == "":
+            return redirect("/login")
+        password = request.form["pass"]
         results = Main.login(user, password)
         if results == 404:
             reset_message()
-            session['error'] = f'User: {user} not found!'
-            return redirect('/login')
+            session["error"] = f"User: {user} not found!"
+            return redirect("/login")
         elif results == 401:
             reset_message()
-            session['error'] = f'Password for user is incorrect!'
-            return redirect('/login')
-        else: 
-            response = make_response(redirect('/'))
-            response.set_cookie('Credentials', bytes(dumps({'User': user, 'Password': password}), encoding='utf-8'))
+            session["error"] = f"Password for user is incorrect!"
+            return redirect("/login")
+        else:
+            response = make_response(redirect("/"))
+            response.set_cookie(
+                "Credentials",
+                bytes(dumps({"User": user, "Password": password}), encoding="utf-8"),
+            )
             return response
 
-    elif method == 'Register Now':
+    elif method == "Register Now":
         reset_message()
-        user = request.form['user']
-        password = request.form['pass']
+        user = request.form["user"]
+        password = request.form["pass"]
         results = Main.register(user, password)
         for response in [
-            (302, 'error', f'Username: {user} already exists! Please use a different username.', '/register'),
-            (201, 'success', 'Successfully registered! You can log in now.', '/'),
-            (404, 'error', 'There was an internal server error! Please try again later', '/register')
-            ]:
+            (302, "error", f"Username: {user} already exists! Please use a different username.", "/register"),
+            (201, "success", "Successfully registered! You can log in now.", "/"),
+            (404, "error", "There was an internal server error! Please try again later", "/register"),
+        ]:
             if results == response[0]:
                 session[response[1]] = response[2]
                 return redirect(response[3])
