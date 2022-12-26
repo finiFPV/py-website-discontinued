87a7b26af5700a32a0c51d24d04e667b654ad0d6
fininimini <markusskalenda@gmail.com>
2022-12-26 20:36:29 +0200

    bugfixes and compatabilty

--- website launcher/website_launcher.py
+++ website launcher/website_launcher.py
@@ -2,27 +2,28 @@ def download_website_files():
     from github import Github
     from os import path, makedirs
     from shutil import rmtree
+    from os import getcwd
 
     repo = Github('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').get_user().get_repo('website')
     branch = 'development'
     contents = repo.get_contents('/Website', ref=branch)
-    main_path = '~\\website'
+    main_path = f'{getcwd()}/website'
     media_filetypes = ('.png', '.jpeg', '.ico', '.gif')
 
     def get(contents, sys_path):
         for content in contents:
             if content.type == 'dir':
-                new_sys_path = f'{sys_path}\\{content.name}'
+                new_sys_path = f'{sys_path}/{content.name}'
                 makedirs(new_sys_path)
                 new_contents = repo.get_contents(content.path, ref=branch)
                 get(new_contents, new_sys_path)
             elif content.name.endswith(media_filetypes):
                 for filetype in media_filetypes:
                     if content.name.endswith(filetype):
-                        with open(f'{sys_path}\\{content.name}', 'wb') as content_file:
+                        with open(f'{sys_path}/{content.name}', 'wb') as content_file:
                             content_file.write(content.decoded_content)
             else:
-                with open(f'{sys_path}\\{content.name}', 'w') as content_file:
+                with open(f'{sys_path}/{content.name}', 'w') as content_file:
                     content_file.write(content.decoded_content.decode('utf-8'))
 
     if path.exists(main_path):
@@ -33,9 +34,13 @@ def download_website_files():
 
 if __name__ == '__main__':
     from subprocess import call
-
-    call(['python', '-m', 'pip', 'install', '--upgrade', 'pip'])
+    from os import getcwd
+    from shutil import copy2
+    
+    call(['python3', '-m', 'pip', 'install', '--upgrade', 'pip'])
     call(['pip', 'install', 'PyGithub'])
     download_website_files()
-    call(['pip', 'install', '-r', '~\\website\\requirments.txt'])
-    call(['python', '~\\website_code\\Main.py'])
+    call(['pip', 'install', '-r', f'{getcwd()}/website/requirments.txt'])
+    copy2(f'{getcwd()}/website/nginx.conf', '/etc/nginx/nginx.conf')
+    call(['systemctl', 'reload', 'nginx'])
+    call(['python3', f'{getcwd()}/website/Main.py'])
