b56e7b881d89835bb4fcbd1ebb5480920dba4a7e
fininimini <markusskalenda@gmail.com>
2022-12-27 15:25:52 +0200

    password hashing

--- Website/functions.py
+++ Website/functions.py
@@ -1,9 +1,13 @@
 from pymongo import MongoClient
+from hashlib import pbkdf2_hmac
+from hmac import compare_digest
 from datetime import datetime
+from os import urandom
 
 class DB:
     def __init__(self, collection="Users"):
         self.client = MongoClient("***REMOVED***")["Website"][collection]
+
     def search_user(self, username):
         results = self.client.find({"user": username})
         results = [credentials for credentials in results]
@@ -14,12 +18,15 @@ class DB:
             user_found = False
             credentials = {}
         return user_found, credentials
+
     def update_user(self, username, field, data):
         self.client.update_one({'user': username}, {'$set': {field: data}})
+
     def insert_data(self, username, password):
+        salt = urandom(16)
         self.client.insert_one({
             "user": username,
-            "pswd": password,
+            "pswd": {"hash": pbkdf2_hmac('sha256', password.encode(), salt, 100000), "salt": salt},
             "membership": None,
             "admin": False,
             "owner": False,
@@ -33,9 +40,10 @@ class Account:
         @staticmethod
         def login(username, password):
             results = DB().search_user(username)
-            if results[0] == False:
+            pswd = results[1]["pswd"]
+            if results[0] is False:
                 return 404
-            elif results[1]["pswd"] == password:
+            elif compare_digest(pswd['hash'], pbkdf2_hmac('sha256', password.encode(), pswd['salt'], 100000)):
                 DB().update_user(username, "last_login", datetime.now().strftime("%Y.%m.%d %H:%M:%S"))
                 return results
             else:
